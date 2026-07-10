624bbbb4b1849df47bfbf41ee2f71f9d6aef9eb1
Matheus <matheusim27@hotmail.com>
2026-07-10 09:48:57 -0300

    add transactions routes and middleware for session ID validation; include @fastify/cookie dependency

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import fastify from 'fastify';
-import { knex } from './database';
+import cookie from '@fastify/cookie'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions';
 
 const app = fastify();
 
-app.get('/hello', async () => {
-  const transactions = await knex('transactions').select('*');
-
-  return transactions;
-});
+app.register(cookie)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
 
 app.listen({ port: env.PORT, }).then(() => {
   console.log('HTTP Server Running!');
